770e63c2033200e632a3621f65ac5d1ec38b838a
majo06005 <majo06005@E-PW0234K2>
2023-10-04 10:21:07 +0200

    Add project files.

---
+++ Uppgift 3.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            string åldertext = Console.ReadLine();
+            int ålder = int.Parse(åldertext);
+
+            switch (ålder)
+            {
+                case 16:
+                    {
+                        Console.WriteLine("Du får vara med i tävlingen");
+                        break;
+                    }
+                case 17:
+                    {
+                        Console.WriteLine("Du får vara med i tävlingen");
+                        break;
+                    }
+                case 18:
+                    {
+                        Console.WriteLine("Du får vara med i tävlingen");
+                        break;
+                    }
+                case 19: 
+                    {
+                        Console.WriteLine("Du får vara med i tävlingen");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Du är fel ålder för att vara med.");
+                        break;
+                    }
+
+            }
+          Console.ReadLine();
+        }
+    }
+}
